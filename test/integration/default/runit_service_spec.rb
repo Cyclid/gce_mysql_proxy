050cef23f317a9fdfcc418906e01928953162561
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-05-22 13:20:17 +0100

    Fix Rubocop

--- test/integration/default/runit_service_spec.rb
+++ test/integration/default/runit_service_spec.rb
@@ -5,6 +5,6 @@ end
 # Can't use the service matcher for Runit services
 describe command('sv status cloud_sql_proxy') do
   its('stdout') do
-    should match %r{\Adown: cloud_sql_proxy: .s, normally up, want up; run: log:.*\Z}
+    should match(/\Adown: cloud_sql_proxy: .s, normally up, want up; run: log:.*\Z/)
   end
 end
